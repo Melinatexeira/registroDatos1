be6f73929924f90cc225ccbad1c677c3770039e3
Melinatexeira <117683416+Melinatexeira@users.noreply.github.com>
2022-11-21 18:38:44 -0300

    Update Registro_persona.java

--- src/Proyecto02/Registro_persona.java
+++ src/Proyecto02/Registro_persona.java
@@ -1,2 +1,27 @@
-package Proyecto02;public class Registro_persona {
+package Proyecto02;
+public class Registro_persona {
+      public static void main(String[] args) {
+
+        System.out.println("**** MENÚ ****");
+        Scanner entradaEscaner = new Scanner(System.in);
+
+        String fname, lname;
+
+
+        System.out.println("Ingrese nombre:");
+        fname = entradaEscaner.nextLine();
+        System.out.println("Ingrese apellido:");
+        lname = entradaEscaner.nextLine();
+
+        System.out.println("Ingrese edad:");
+        int age = entradaEscaner.nextInt();
+
+        if (age >= 18) {
+            System.out.println(fname + " " + lname + " " + ": Puede pasar a la fiesta. ¡Diviértase!");
+
+        } else {
+            System.out.println(fname + " " + lname + " " + " : ¡Usted es menor de edad, no tiene el acceso permitido!");
+        }
+    }
+}
 }
